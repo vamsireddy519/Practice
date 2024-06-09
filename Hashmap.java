7bf7d345f72fa251b58485fe1fc3e61c46fbfcab
vamsireddy519 <vamsikarna519@gmail.com>
2024-06-09 14:06:32 +0530

    Add files via upload

---
+++ Hashmap.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class A{
+    public static void main(String args[]){
+        HashMap<Integer,String> a1=new HashMap<>();
+        a1.put(5,"sai");
+        a1.put(3,"rohitsdf");
+        a1.put(99,"ko");
+        a1.put(15,"surya");
+        a1.put(1,"non");
+        System.out.println(a1);
+}
+}
